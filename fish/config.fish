b4918553758f3efda07ff67f5c29d320019f52f5
ziad mostafa <ziadmostafa760@gmail.com>
2025-01-07 16:29:26 +0200

    new update for waybar also add ctrl+d in fish config to search with fzf fro dirs

--- fish/config.fish
+++ fish/config.fish
@@ -5,15 +5,6 @@ if status is-interactive
 end
 
 
-
-
-function search_files
-    set file (find . -type f | fzf)
-    if test -n "$file"
-        nvim "$file"
-    end
-end
-
 function fzf_cd
     # Search for directories starting from ~/
     set -l dir (find ~/ -type d 2>/dev/null | fzf --height 40% --reverse --prompt="Select directory: ")
@@ -28,20 +19,9 @@ end
 bind \cd fzf_cd
 
 
-
-# List Directory
-alias l='eza -lh  --icons=auto' # long list
-alias ls='eza -1   --icons=auto' # short list
-alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
-alias ld='eza -lhD --icons=auto' # long list dirs
-alias lt='eza --icons=auto --tree' # list folder as tree
-
-# Handy change dir shortcuts
-abbr .. 'cd ..'
-abbr ... 'cd ../..'
-abbr .3 'cd ../../..'
-abbr .4 'cd ../../../..'
-abbr .5 'cd ../../../../..'
-
-# Always mkdir a path (this doesn't inhibit functionality to make a single dir)
-abbr mkdir 'mkdir -p'
+function search_files
+    set file (find . -type f | fzf)
+    if test -n "$file"
+        nvim "$file"
+    end
+end
--- spotify/Users/31mnlcr4cac7o5vces444e2kw6z4-user/frecency.pb
+++ spotify/Users/31mnlcr4cac7o5vces444e2kw6z4-user/frecency.pb
@@ -1,5 +1,24 @@
 
+Y
+Bspotify:user:31mnlcr4cac7o5vces444e2kw6z4:collection:your-episodes	Ò“(ç
+˜y@ ™¬Æ»
 >
-'spotify:playlist:37i9dQZF1DXaGui85Swluy	„#¿ *y@ —ëø¸
+'spotify:playlist:37i9dQZF1DXaGui85Swluy	ı¼³‘¦y@	 —ëø¸
+>
+'spotify:playlist:4VikF4kqet9vEC4KtnNe4m	OĞ3áªy@ ã›‹»
+>
+'spotify:playlist:3rITtpqpfSBWq9srjAq7BB	Şİ49®y@ ¤«Æ»
+>
+'spotify:playlist:37i9dQZF1DX4qF0846GNk8	V<åë6®y@ ˆšÆ»
+>
+'spotify:playlist:1eiO2K0C4bfgqJBS139Z6D	ŠŠ9®y@ Ş«Æ»
+>
+'spotify:playlist:0urJ5vxo1uVuqe5GfUhP3f	’»ò8®y@ ¹ªÆ»
+>
+'spotify:playlist:37i9dQZF1FgnTBfUlzkeKt	<ÊzZ:®y@ çµÆ»
+>
+'spotify:playlist:3VF35n8O2sG9LjMo9kPXRq	˜–yó8®y@ ¼ªÆ»
+>
+'spotify:playlist:37i9dQZF1E38zPs8CvhCeA	Qp8õ‡®y@ ©Ë»
 `
-4spotify:user:31mnlcr4cac7o5vces444e2kw6z4:collection	Jãş”y@ ˜†»	1}kàûËy@! ƒ»†»
+4spotify:user:31mnlcr4cac7o5vces444e2kw6z4:collection	ÏQŞ®¨šy@ «Òğ»	ZÉyò‹ßy@U ­ìô»
